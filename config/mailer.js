43e742dd32958e55b4e6ecf13dd0455ef3eef378
Mpho Lebona <giftedmpho99@gmail.com>
2026-02-10 12:49:37 +0200

    Enable debug mode on nodemailer config file to observer email delivery issues

--- config/mailer.js
+++ config/mailer.js
@@ -3,12 +3,15 @@ import { USER_SMTP_PASS, ADMIN_EMAIL } from "./env.js";
 
 
 const transporter = nodemailer.createTransport({
+    service: 'gmail',
     host: "smtp.gmail.com",
     secure: true,
     auth: {
         user: ADMIN_EMAIL, // Avoid explicitly declaring the email in the codebase.
         pass: USER_SMTP_PASS,
     },
+    logger: true, // Log information to console
+    debug: true,  // Include SMTP traffic in the logs
 });
 
 export default transporter;
