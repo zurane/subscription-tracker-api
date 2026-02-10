d0b2ce1321b79da7cb786668c570be49cdf07412
Mpho Lebona <giftedmpho99@gmail.com>
2026-02-10 14:59:20 +0200

    Fix: Update mailOptions to set from to resend domain

--- utils/send-email.js
+++ utils/send-email.js
@@ -4,41 +4,42 @@ import transporter from "../config/mailer.js";
 import { ADMIN_EMAIL } from "../config/env.js";
 
 const sendEmailReminder = async ({ to, type, subscription }) => {
-  //check if there it to and type value otherwise throw an error
+    //check if there it to and type value otherwise throw an error
 
-  if (!to || !type) {
-    throw new Error("missing required parameters");
-  }
-  const template = emailTemplates.find((t) => t.label === type);
+    if (!to || !type) {
+        throw new Error("missing required parameters");
+    }
+    const template = emailTemplates.find((t) => t.label === type);
 
-  if (!template) {
-    throw new Error("No template found");
-  }
+    if (!template) {
+        throw new Error("No template found");
+    }
 
-  const mailInfo = {
-    userName: subscription.user.name,
-    subscriptionName: subscription.name,
-    renewalDate: dayjs(subscription.renewalDate).format("MM/DD/YYYY"),
-    price: `${subscription.currency} ${subscription.price}`,
-    frequency: subscription.subscriptionFrequency,
-    status: subscription.status,
-    paymentMethod: subscription.paymentMethod,
-  };
+    const mailInfo = {
+        userName: subscription.user.name,
+        subscriptionName: subscription.name,
+        renewalDate: dayjs(subscription.renewalDate).format("MM/DD/YYYY"),
+        price: `${subscription.currency} ${subscription.price}`,
+        frequency: subscription.subscriptionFrequency,
+        status: subscription.status,
+        paymentMethod: subscription.paymentMethod,
+    };
 
-  const message = template.generateBody(mailInfo);
-  const subject = template.generateSubject(mailInfo);
+    const message = template.generateBody(mailInfo);
+    const subject = template.generateSubject(mailInfo);
 
-  const mailOptions = {
-    from: ADMIN_EMAIL,
-    to: to,
-    subject: subject,
-    html: message,
-  };
+    const mailOptions = {
+        from: 'onboarding@resend.dev',
+        to: to,
+        subject: subject,
+        html: message,
+        replyTo: 'giftedmpho99@gmail.com'
+    };
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) return console.log(error);
-    res.status(200).json({ message: info.response });
-  });
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) return console.log(error);
+        res.status(200).json({ message: info.response });
+    });
 };
 
 export default sendEmailReminder;
